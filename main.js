636c3c8762cdd8ae8c88881d0a145caeebbc2c83
nocriteriaforaevaaaaa <aevaacharya9@gmail.com>
2024-06-12 23:44:07 +0545

    new

---
+++ main.js
@@ -0,0 +1,29 @@
+var height = 6; 
+var width = 5; 
+
+var row = 0; 
+var col = 0; 
+
+var gameOver = false;
+var word = "SQUID";
+
+
+window.onload = function(){
+    intialize();
+}
+
+
+function intialize() {
+
+    
+    for (let r = 0; r < height; r++) {
+        for (let c = 0; c < width; c++) {
+            
+            let tile = document.createElement("span");
+            tile.id = r.toString() + "-" + c.toString();
+            tile.classList.add("tile");
+            tile.innerText = "";
+            document.getElementById("board").appendChild(tile);
+        }
+    }
+}
